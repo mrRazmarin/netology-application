addf7b04c8cd06748cdaebecbaefd4707c839056
Mikhail Salnikov <salnikovmishanew@gmail.com>
2026-06-10 10:01:17 +0300

    Домашка "Intents и организация навигации" задание "Editing"

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -15,15 +15,19 @@ import ru.netology.nmedia.utils.AndroidUtils.applySystemBarsPadding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val postViewModel: PostViewModel by viewModels()
+    private val editContract = registerForActivityResult(EditPostContract) { editResult ->
+        postViewModel.save(editResult)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        val postViewModel: PostViewModel by viewModels()
-        val postContract = registerForActivityResult(NewPostContract) {
-            result ->
+        val postContract = registerForActivityResult(NewPostContract) { result ->
             postViewModel.save(result)
         }
+
         val adapter = PostAdapter(createPostListener(postViewModel))
 
         setContentView(mainBinding.root)
@@ -52,7 +56,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun createPostListener(postViewModel: PostViewModel): PostListener  {
+    private fun createPostListener(postViewModel: PostViewModel): PostListener {
         return object : PostListener {
             override fun onLike(post: Post) {
                 postViewModel.likeById(post.id)
@@ -80,6 +84,9 @@ class MainActivity : AppCompatActivity() {
 
             override fun onEdit(post: Post) {
                 postViewModel.editPostById(post)
+                val postContent = post.content
+
+                editContract.launch(postContent)
             }
         }
     }
--- app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
@@ -5,11 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.ActivityNewPostBinding
 import ru.netology.nmedia.utils.AndroidUtils.applySystemBarsPadding
 
